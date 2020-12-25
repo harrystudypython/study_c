040944042ad21b71861c488feceaccc908373594
harry_shen <harry_shen@ymtc.com>
2020-12-25 11:38:15 +0800

    amazing

---
+++ calc_priority.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+printf("hello ,world!\n");
+int a;
+a=1;
+int plus_plus_a;
+plus_plus_a=a++;
+printf ("a is %d \n",a);
+printf ("plus_plus_a is %d \n",plus_plus_a);
+
+printf ("bye,world");
+
+return 0;
+
+}
